67b512150885d07bc1484563cd6f5fe2335e1761
Harumi Terayama <hterayama@gmail.com>
2021-03-01 15:01:38 +0100

    iteration 14 display buttons

--- src/components/FaceBook.jsx
+++ src/components/FaceBook.jsx
@@ -6,37 +6,65 @@ class Facebook extends React.Component {
     super();
     this.state = {
       profiles: profiles,
+      selectedCountry: '',
     };
   }
+
+  setCountry = (input) => {
+    this.setState({ selectedCountry: input });
+  };
+
   render() {
+    let countryButtons = ['All'];
+
+    for (let profile of this.state.profiles) {
+      if (!countryButtons.find((country) => country === profile.country)) {
+        countryButtons.push(profile.country);
+      }
+    }
+
     return (
-      <ul>
-        {this.state.profiles.map((profile) => {
-          return (
-            <div className="single-id-card">
-              <img
-                className="id-picture"
-                src={profile.img}
-                alt={(profile.firstName, profile.lastName)}
-              />
-              <div className="id-info">
-                <p>
-                  <b>First name:</b> {profile.firstName}
-                </p>
-                <p>
-                  <b>Last name:</b> {profile.lastName}
-                </p>
-                <p>
-                  <b>Country:</b> {profile.country}
-                </p>
-                <p>
-                  <b>Type:</b> {(profile.isStudent && 'Student') || 'Teacher'}
-                </p>
+      <div>
+        <div className="country-buttons">
+          {countryButtons.map((country) => {
+            return (
+              <button
+                onClick={this.setCountry}
+                className="single-country-button"
+              >
+                {country}
+              </button>
+            );
+          })}
+        </div>
+        <ul>
+          {this.state.profiles.map((profile) => {
+            return (
+              <div className="single-id-card">
+                <img
+                  className="id-picture"
+                  src={profile.img}
+                  alt={(profile.firstName, profile.lastName)}
+                />
+                <div className="id-info">
+                  <p>
+                    <b>First name:</b> {profile.firstName}
+                  </p>
+                  <p>
+                    <b>Last name:</b> {profile.lastName}
+                  </p>
+                  <p>
+                    <b>Country:</b> {profile.country}
+                  </p>
+                  <p>
+                    <b>Type:</b> {(profile.isStudent && 'Student') || 'Teacher'}
+                  </p>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </ul>
+            );
+          })}
+        </ul>
+      </div>
     );
   }
 }
